fc51c7bcc00da5a86930eb06921834c23e6547c0
yoshihide <threadSleep(100)>
2016-11-14 20:42:46 +0900

    StringUtility更新

--- app/src/main/java/com/so/okamnk/alarmclock/util/StringUtility.java
+++ app/src/main/java/com/so/okamnk/alarmclock/util/StringUtility.java
@@ -1,8 +1,22 @@
 package com.so.okamnk.alarmclock.util;
 
 /**
- * Created by araiyuuichi on 2016/11/11.
+ * 文字列操作のユーティリティクラス
+ * Created by kato on 2016/11/14.
  */
 
 public class StringUtility {
+
+    /**
+     * 指定文字列が、nullもしくは空文字かの判定します
+     *
+     * @param str 指定文字列
+     * @return nullか空文字であればtrueを、そうでない場合はfalseを返す
+     */
+    public static boolean isNullOrEmpty(String str) {
+        if (str == null || str.isEmpty() == true) {
+            return true;
+        }
+        return false;
+    }
 }
