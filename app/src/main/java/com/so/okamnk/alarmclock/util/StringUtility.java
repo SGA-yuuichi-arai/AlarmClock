3626da7274a2908f8f9153017209c3f04e1555ea
yoshihide <threadSleep(100)>
2016-11-19 21:44:56 +0900

    以下の関数を追加 ・String > Date 変換関数 ・Date > String 変換関数

--- app/src/main/java/com/so/okamnk/alarmclock/util/StringUtility.java
+++ app/src/main/java/com/so/okamnk/alarmclock/util/StringUtility.java
@@ -1,11 +1,17 @@
 package com.so.okamnk.alarmclock.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * 文字列操作のユーティリティクラス
  * Created by kato on 2016/11/14.
  */
 
 public class StringUtility {
+
+    private static String DATE_FOMAT = "yyyyMMddTHHmmss";
 
     /**
      * 指定文字列が、nullもしくは空文字かの判定します
@@ -18,5 +24,29 @@ public class StringUtility {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Date型の日時を文字列(yyyyMMddTHHmmss)に変換します
+     *
+     * @param date Date型の指定日時
+     * @return yyyyMMddTHHmmssフォーマットの文字列
+     */
+    public static String convertDateToString(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("DATE_FOMAT");
+        return formatter.format(date);
+    }
+
+    /**
+     * 文字列で表現された日付(yyyyMMddTHHmmss)を、Date型に変換します<br>
+     * フォーマットが異なる場合、ParseExceptionをthrowします
+     *
+     * @param dateStr 文字列の日付
+     * @return Date型の指定日時
+     * @throws ParseException
+     */
+    public static Date convertStringToDate(String dateStr) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("DATE_FOMAT");
+        return formatter.parse(dateStr);
     }
 }
