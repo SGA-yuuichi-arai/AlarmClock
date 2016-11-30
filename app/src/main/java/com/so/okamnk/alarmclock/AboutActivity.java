9c1b271fb2e7b68795fdf91d6b68df24090c6ebc
a.kinoshita <akira_kinoshita@m.sobal.co.jp>
2016-11-30 23:35:02 +0900

    担当画面の追加(Activityとxml) strings.xmlの更新

--- app/src/main/java/com/so/okamnk/alarmclock/AboutActivity.java
+++ app/src/main/java/com/so/okamnk/alarmclock/AboutActivity.java
@@ -1,8 +1,88 @@
 package com.so.okamnk.alarmclock;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 /**
  * Created by araiyuuichi on 2016/11/11.
  */
 
-public class AboutActivity {
+public class AboutActivity  extends AppCompatActivity implements View.OnClickListener {
+
+    private String version;
+    private String copyright;
+
+    public static ImageButton imageButton_back;
+    public static TextView textView_aboutApplication, textView_Version, textView_copyright, textView_openSourceLicense;
+    public static WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        //ActionBar actionBar = getActionBar();
+        //actionBar.hide();
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setTitle("アプリについて");
+
+        imageButton_back = new ImageButton(this);
+        textView_aboutApplication = new TextView(this);
+        textView_Version = new TextView(this);
+        textView_copyright = new TextView(this);
+        textView_openSourceLicense = new TextView(this);
+        webview = new WebView(this);
+
+        imageButton_back = (ImageButton)findViewById(R.id.imageButton_back);
+        textView_aboutApplication = (TextView)findViewById(R.id.textView_aboutApplication);
+        textView_Version = (TextView)findViewById(R.id.textView_Version);
+        textView_copyright = (TextView)findViewById(R.id.textView_copyright);
+        textView_openSourceLicense = (TextView)findViewById(R.id.textView_openSourceLicense);
+        webview = (WebView)findViewById(R.id.webview);
+
+        imageButton_back.setOnClickListener(this);
+
+
+        //textView_Version.setText(packageInfo.versionName);
+
+        // ActionBar actionBar = getActionBar();
+        // actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+
+            case R.id.imageButton_back:
+
+                startActivity(new Intent(AboutActivity.this, AlarmRegistActivity.class));
+                break;
+
+        }
+    }
+
+    private void displayView(){
+
+        // PackageManager pm = new PackageManager();
+        PackageManager pm;
+        // PackageInfo packageInfo = pm.getPackageInfo(String packageName, int flags);
+
+        // gpm = new PackageManager().getPackageInfo(getPackageName(), PackageManager.GET_META_DATA);
+        //ActivityInfo gpm = pm.getPackageInfo(getPackageName(), PackageManager.GET_META_DATA);
+        //copyright = gpm.metaData.getString("copyright");
+
+        //textView_Version.setText(packageInfo.versionName);
+
+
+    }
+
 }
+
--- app/src/main/java/com/so/okamnk/alarmclock/AlarmRegistActivity.java
+++ app/src/main/java/com/so/okamnk/alarmclock/AlarmRegistActivity.java
@@ -1,8 +1,105 @@
 package com.so.okamnk.alarmclock;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
 /**
  * Created by araiyuuichi on 2016/11/11.
  */
 
-public class AlarmRegistActivity {
+public class AlarmRegistActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static ImageButton imageButton_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_resist);
+
+        //ActionBar actionBar = getActionBar();
+        //actionBar.hide();
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setTitle("アラーム登録");
+
+        imageButton_back = new ImageButton(this);
+
+        imageButton_back = (ImageButton) findViewById(R.id.imageButton_back);
+        imageButton_back.setOnClickListener(this);
+
+
+        ((Spinner) findViewById(R.id.spinner_pattern)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        ((Spinner) findViewById(R.id.spinner_interval)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        ((Spinner) findViewById(R.id.spinner_times)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        /*
+        ArrayAdapter adapter = new ArrayAdapter(this, spinner_mannerMode);
+        adapter.add("音を出す");
+        adapter.add("バイブレーション");
+        adapter.setDropDownViewResource(spinner_mannerMode);
+        spinner_mannerMode = (Spinner) findViewById(spinner_mannerMode);
+        spinner_mannerMode.setAdapter(adapter);
+        */
+
+        // Spinnerの項目選択時のリスナー
+        ((Spinner) findViewById(R.id.spinner_mannerMode)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 選択された文字列を取得
+                String str = getResources().getStringArray(R.array.mannerMode)[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+    }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.imageButton_back:
+
+                startActivity(new Intent(AlarmRegistActivity.this, AboutActivity.class));
+                break;
+
+        }
+
+    }
+
 }
