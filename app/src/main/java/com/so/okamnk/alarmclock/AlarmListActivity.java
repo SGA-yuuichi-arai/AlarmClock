3b447dfa84e6b6edfc9dc1fd9d73b963235e8ff4
yyy-arai <yuuichi_arai@m.sobal.co.jp>
2016-12-18 13:49:27 +0900

    AlarmListActivity実装 リスト用にAlatmListAdapterを追加 使用するレイアウト、リソースを追加

--- app/src/main/java/com/so/okamnk/alarmclock/AlarmListActivity.java
+++ app/src/main/java/com/so/okamnk/alarmclock/AlarmListActivity.java
@@ -1,13 +1,88 @@
 package com.so.okamnk.alarmclock;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.so.okamnk.alarmclock.util.AlarmDBAdapter;
+import com.so.okamnk.alarmclock.util.AlarmEntity;
+
+import java.util.ArrayList;
 
 public class AlarmListActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter<AlarmEntity> adapter;
+    private ArrayList<AlarmEntity> entities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_list);
+
+        if (savedInstanceState == null) {
+            listView = (ListView) findViewById(R.id.alarm_list);
+        }
+
+        adapter = new AlarmListAdapter(getApplicationContext());
+        entities = new ArrayList<>();
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AlarmDBAdapter dbAdapter = new AlarmDBAdapter(getApplicationContext());
+//        entities.addAll(dbAdapter.getAll());
+
+        // test
+        entities.clear();
+        entities.add(new AlarmEntity(0, "2016/12/17/20:23", "test", "20:27", "/tmp/test", 0,
+                100, 0, 0, 0, 3, true,
+                false, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(1, "2016/12/17/20:23", "test2", "20:27", "/tmp/test2", 0,
+                100, 0, 0, 0, 3, true,
+                true, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(0, "2016/12/17/20:23", "test", "20:27", "/tmp/test", 0,
+                100, 0, 0, 0, 3, true,
+                false, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(1, "2016/12/17/20:23", "test2", "20:27", "/tmp/test2", 0,
+                100, 0, 0, 0, 3, true,
+                true, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(0, "2016/12/17/20:23", "test", "20:27", "/tmp/test", 0,
+                100, 0, 0, 0, 3, true,
+                false, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(1, "2016/12/17/20:23", "test2", "20:27", "/tmp/test2", 0,
+                100, 0, 0, 0, 3, true,
+                true, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(0, "2016/12/17/20:23", "test", "20:27", "/tmp/test", 0,
+                100, 0, 0, 0, 3, true,
+                false, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(1, "2016/12/17/20:23", "test2", "20:27", "/tmp/test2", 0,
+                100, 0, 0, 0, 3, true,
+                true, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(0, "2016/12/17/20:23", "test", "20:27", "/tmp/test", 0,
+                100, 0, 0, 0, 3, true,
+                false, false, false, false,
+                false, false, true));
+        entities.add(new AlarmEntity(1, "2016/12/17/20:23", "test2", "20:27", "/tmp/test2", 0,
+                100, 0, 0, 0, 3, true,
+                true, false, false, false,
+                false, false, true));
+
+        adapter.clear();
+        adapter.addAll(entities);
+        adapter.notifyDataSetChanged();
+
     }
 }
