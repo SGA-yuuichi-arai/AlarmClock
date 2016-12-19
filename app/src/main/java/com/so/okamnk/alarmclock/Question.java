63637123abb19207fb602c6d60b646d15ea5f50b
mutoua <naoto_mutou@m.sobal.co.jp>
2016-12-19 21:51:54 +0900

    Questionクラスの実装

--- app/src/main/java/com/so/okamnk/alarmclock/Question.java
+++ app/src/main/java/com/so/okamnk/alarmclock/Question.java
@@ -1,8 +1,29 @@
 package com.so.okamnk.alarmclock;
 
-/**
- * Created by araiyuuichi on 2016/11/11.
- */
+import java.util.Random;
 
 public class Question {
+    private String mQuestion;
+    private String mAnswer;
+
+    public static Question createRandomQuestion() {
+        // 「a + b = c」 という問題を作る
+        Random random = new Random();
+        int a = random.nextInt(90) + 10;
+        int b = random.nextInt(90) + 10;
+        int c = a + b;
+
+        Question obj = new Question();
+        obj.mQuestion = "" + a + "+" + b + "=";
+        obj.mAnswer = "" + c;
+        return obj;
+    }
+
+    public String getQuestionString() {
+        return mQuestion;
+    }
+
+    public boolean isCorrectAnswer(String answer) {
+        return mAnswer.equals(answer);
+    }
 }
